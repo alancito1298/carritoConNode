212dc1334fc66f8f309aa13085a9c78f6804bd21
Alan <alangerman98@yahoo.com.ar>
2024-08-07 16:02:37 -0300

    conexion a base de datos

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+const database = require("./database")
 const express = require("express");
 const morgan = require("morgan");
 
 //confg inicial
 const app = express();
-app.set("port",4000);
+app.set("port",4006);
 app.listen(app.get("port"));
 console.log("escuchando puerto" + app.get("port"));
 
@@ -15,8 +16,31 @@ app.use(morgan("dev"));
 
 
 
-app.get("/productos", (req, res) => {
-    res.send("mensaje recibido ok")
-}
+app.get("/productos", async (req, res) => {
+    const connection = await database.getConnection();
+    const [resultado] = await connection.query("SELECT * FROM `productos en stock` WHERE ID = 1");
+    
+    
+    res.send("mensaje recibido ok " + resultado.nombre);
+   
+    
+} 
 
 )
+/* 
+const mysql = require("promise-mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connection = mysql.createConnection({
+
+host: "localhost",
+port: 3308,
+database:"carrito de compras",
+user: "root",
+password: ""
+})
+
+
+
+const getConnection = async () => await connection;*/
